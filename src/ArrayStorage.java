1f54ae35a1140e45916f8a343b66b64844646e28
Igor <traver.unimmer@gmail.com>
2023-10-09 21:50:32 +0300

    feat: complete save method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,15 +6,24 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
-    void clear() {}
+    void clear() {
+    }
 
-    void save(Resume r) {}
+    void save(Resume r) {
+        for (int i = storage.length - 1; i > 0; i--) {
+            Resume tmp = storage[i - 1];
+            storage[i - 1] = storage[i];
+            storage[i] = tmp;
+        }
+        storage[0] = r;
+    }
 
     Resume get(String uuid) {
         return null;
     }
 
-    void delete(String uuid) {}
+    void delete(String uuid) {
+    }
 
     /**
      * @return array, contains only Resumes in storage (without null)
