99d3f98b9ee08ba3a744b1c8aa93ad6ff9797da1
Igor <traver.unimmer@gmail.com>
2023-10-09 21:27:01 +0300

    init: start lesson1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,27 +1,26 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
-    void clear() {
-    }
+    void clear() {}
 
-    void save(Resume r) {
-    }
+    void save(Resume r) {}
 
     Resume get(String uuid) {
         return null;
     }
 
-    void delete(String uuid) {
-    }
+    void delete(String uuid) {}
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return null;
     }
 
     int size() {
