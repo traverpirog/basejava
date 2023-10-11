de4f84403a38007aeef466515749bed874bae2c8
Igor <traver.unimmer@gmail.com>
2023-10-11 09:47:43 +0300

    feat: create isResume,update method | add isResume checker for methods (get, save, delete, update)

--- src/com/javaops/webapp/storage/ArrayStorage.java
+++ src/com/javaops/webapp/storage/ArrayStorage.java
@@ -17,19 +17,48 @@ public class ArrayStorage {
     }
 
     public void save(Resume r) {
-        storage[size] = r;
-        size++;
+        if (isResume(r.getUuid())) {
+            System.out.println("ERROR: Резюме " + r.getUuid() + " уже существует!");
+            return;
+        }
+
+        if (storage.length > size) {
+            storage[size] = r;
+            size++;
+            return;
+        }
+
+        System.out.println("ERROR: storage переполнен");
     }
 
     public void update(Resume r) {
+        if (!isResume(r.getUuid())) {
+            System.out.println("ERROR: Резюме " + r.getUuid() + " не найдено!");
+            return;
+        }
 
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(r.getUuid())) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     public Resume get(String uuid) {
-        return Arrays.stream(storage).limit(size).filter(resume -> resume.getUuid().equals(uuid)).findFirst().orElse(null);
+        if (!isResume(uuid)) {
+            System.out.println("ERROR: Резюме " + uuid + " не найдено!");
+            return null;
+        }
+        return Arrays.stream(storage).limit(size).filter(resume -> resume.getUuid().equals(uuid)).findFirst().get();
     }
 
     public void delete(String uuid) {
+        if (!isResume(uuid)) {
+            System.out.println("ERROR: Резюме " + uuid + " не найдено!");
+            return;
+        }
+
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 storage[i] = storage[size - 1];
@@ -38,6 +67,10 @@ public class ArrayStorage {
                 break;
             }
         }
+    }
+
+    private boolean isResume(String uuid) {
+        return Arrays.stream(storage).limit(size).anyMatch(resume1 -> uuid.equals(resume1.getUuid()));
     }
 
     /**
