ec4dffce4f108c104dab6380f8a7d9ee05cdf190
Igor <traver.unimmer@gmail.com>
2023-11-23 09:30:38 +0300

    feat: refactor Deadlock

--- src/com/javaops/webapp/Deadlock.java
+++ src/com/javaops/webapp/Deadlock.java
@@ -6,19 +6,30 @@ public class Deadlock {
         Person person2 = new Person("Tommy");
         new Thread(() -> {
             synchronized (person2) {
+                System.out.println(Thread.currentThread().getName() + ": got person2 monitor");
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+                System.out.println(Thread.currentThread().getName() + ": trying to get person monitor");
                 synchronized (person) {
+                    System.out.println(Thread.currentThread().getName() + ": got person monitor");
                     person2.greeting(person);
                 }
             }
         }).start();
         new Thread(() -> {
             synchronized (person) {
+                System.out.println(Thread.currentThread().getName() + ": got person monitor");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + ": trying to get person2 monitor");
                 synchronized (person2) {
+                    System.out.println(Thread.currentThread().getName() + ": got person2 monitor");
                     person.greeting(person2);
                 }
             }
@@ -27,8 +38,7 @@ public class Deadlock {
 
     record Person(String name) {
         public void greeting(Person person) {
-                System.out.println("Hello, " + person.name());
-            }
+            System.out.println("Hello, " + person.name());
         }
-
+    }
 }
