97085b108cfd94f976298a7216984bc7bae743a7
Igor <traver.unimmer@gmail.com>
2023-12-15 10:02:28 +0300

    feat: refactor SectionType

--- src/com/javaops/webapp/model/SectionType.java
+++ src/com/javaops/webapp/model/SectionType.java
@@ -3,18 +3,8 @@ package com.javaops.webapp.model;
 public enum SectionType {
     PERSONAL("Личные качества"),
     OBJECTIVE("Позиция"),
-    ACHIEVEMENT("Достижения") {
-        @Override
-        public String toHtml(String value) {
-            return value.replaceAll("\n", "<br>");
-        }
-    },
-    QUALIFICATIONS("Квалификация") {
-        @Override
-        public String toHtml(String value) {
-            return value.replaceAll("\n", "<br>");
-        }
-    },
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
     EXPERIENCE("Опыт работы"),
     EDUCATION("Образование");
 
@@ -26,9 +16,5 @@ public enum SectionType {
 
     public String getTitle() {
         return title;
-    }
-
-    public String toHtml(String value) {
-        return (value == null) ? "" : value;
     }
 }
